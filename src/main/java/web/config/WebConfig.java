4c087011b25020b55c124cb1257fbb27aa8d72be
Михаил <mih.borey@gmail.com>
2024-02-16 02:28:13 +0500

    update dependencies: spring 6.1.3, jakarta.servlet 6.0.0, thymeleaf 3.1.2, maven-war-plugin 3.4.0

--- src/main/java/web/config/WebConfig.java
+++ src/main/java/web/config/WebConfig.java
@@ -4,12 +4,13 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.thymeleaf.spring5.SpringTemplateEngine;
-import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
-import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
 
 @Configuration
 @EnableWebMvc
@@ -29,6 +30,8 @@ public class WebConfig implements WebMvcConfigurer {
         templateResolver.setApplicationContext(applicationContext);
         templateResolver.setPrefix("/WEB-INF/pages/");
         templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCacheable(false);
         return templateResolver;
     }
 
@@ -40,11 +43,11 @@ public class WebConfig implements WebMvcConfigurer {
         return templateEngine;
     }
 
-
-    @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
+    @Bean
+    public ViewResolver viewResolver() {
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
         resolver.setTemplateEngine(templateEngine());
-        registry.viewResolver(resolver);
+        resolver.setCharacterEncoding("UTF-8");
+        return resolver;
     }
 }
